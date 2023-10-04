de98420240405ab707ff805372aa71bc8ea8884e
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-10-05 04:42:51 +0700

    Làm trang sửa tài khoản

--- routes/admin/accounts.route.js
+++ routes/admin/accounts.route.js
@@ -13,4 +13,8 @@ router.get("/create",controller.create);
 
 router.post("/create", upload.single("avatar"), uploadStream.upload, validater.creatPost, controller.createPost);
 
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id", upload.single("avatar"), uploadStream.upload, validater.editPatch, controller.editPatch);
+
 module.exports = router;
