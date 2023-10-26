dcf6468bcecb71b5e9c92a7184e8fb131c367765
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-10-27 02:20:22 +0700

    Làm giao diện trang chat

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const searchRouters = require("./search.route");
 const cartRouters = require("./cart.route");
 const checkoutRouters = require("./checkout.route");
 const userRouters = require("./user.route");
+const chatRouters = require("./chat.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const userMiddleware = require("../../middlewares/client/user.middleware");
@@ -21,4 +22,5 @@ module.exports = (app) => {
   app.use("/cart", cartRouters);
   app.use("/checkout", checkoutRouters);
   app.use("/user", userRouters);
+  app.use("/chat", chatRouters);
 };
