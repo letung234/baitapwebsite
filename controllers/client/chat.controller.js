144f78832ac4d2480866a5ba30b17b2a4a0ddfd0
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-03 04:57:36 +0700

    Tối ưu lại folder socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -2,50 +2,14 @@ const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
 const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
 
+const chatSocket = require("../../sockets/client/chat.socket");
 //GET /chat
 
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-  // Socket
-  _io.once("connection", (socket) => {
-    console.log("connect" + socket.id);
-    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-      let images = [];
+  // Socket IO
+  chatSocket(res);
 
-      for (const imageBuffer of data.images) {
-        const link = await uploadToCloudinary(imageBuffer);
-        images.push(link);
-      }
-
-      // Lưu vào dtbase
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        images: images,
-      });
-      await chat.save();
-
-      // Trả data vè client
-      _io.emit("SERVER_RETURN_MESSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        images: images,
-      });
-    });
-    //Typing
-    socket.on("CLIENT_SEND_TYPING", async (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        userId: userId,
-        fullName: fullName,
-        type: type,
-      });
-    });
-    //End Typing
-  });
-
-  // End
+  // End Socket IO
 
   // Lấy data từ database
   const chats = await Chat.find({
@@ -56,9 +20,7 @@ module.exports.index = async (req, res) => {
       _id: chat.user_id,
     }).select("fullName");
     chat.inforUser = inforUser;
-  }
-
-  
+  };
 
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
